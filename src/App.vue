a9a795a69cde74bd66c8a17a24d157f3f4c97368
Minghao <minghao.wang@dbjtech.com>
2018-03-16 13:54:53 +0800

    feat: init vue project

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+      <div class="box">
+          <span class="child">333</span>
+      </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="scss" scoped>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+    .box {
+        width: 50px;
+        height: 50px;
+        background-color: #ccc;
+        span {
+            background-color: #fff;
+        }
+    }
+</style>
